ec40878a920611c84f199f4f76ad6749c9c2cd75
Oceguera <no67wuwu@idiv.de>
2024-01-26 14:59:24 +0100

    New approach for performing habitat changes analysis

---
+++ 01_scrip/natura2000_species_changes.R
@@ -0,0 +1,154 @@
+# Remove all objects from the environment
+rm(list=ls())
+gc()
+
+# Load libraries
+library(sf)
+library(dplyr)
+
+setwd("I:/biocon/Emmanuel_Oceguera/projects/2023_03_ETC_BE/Task 1.1.7.2 Protected areas dataflows/Subtask 2.viii Sufficiency assesment/outputs/PL")
+
+# Define the paths to the data and the layer name
+gpkg_file_2018_path <- "I:/biocon/Emmanuel_Oceguera/projects/2023_03_ETC_BE/Task 1.1.7.2 Protected areas dataflows/Subtask 2.viii Sufficiency assesment/outputs/PL/Natura_2000_sites_v2018/species/natura_2000_PL_species_all_sites_ETRS89.gpkg"
+gpkg_file_2023_path <- "I:/biocon/Emmanuel_Oceguera/projects/2023_03_ETC_BE/Task 1.1.7.2 Protected areas dataflows/Subtask 2.viii Sufficiency assesment/outputs/PL/Natura_2000_sites_v2023/species/natura_2000_PL_species_all_sites_ETRS89.gpkg"
+output_file <- "Changes_Natura2000_sites_v2018_v2023/species"
+
+
+# Get the name fila
+layer_name <- st_layers(gpkg_file_2023_path)$name[1]
+
+
+# Read habitats spatial data 2018 and 2023 for Poland
+natura2000_spe_2018 <- st_read(gpkg_file_2018_path, layer = layer_name) # every time when you run the function set the layers name
+natura2000_spe_2023 <- st_read(gpkg_file_2023_path, layer = layer_name) # every time when you run the function set the layers name
+
+# # Convert to data frames
+# natura2000_spe_2018_df <- as.data.frame(natura2000_spe_2018)
+# natura2000_spe_2023_df <- as.data.frame(natura2000_spe_2023)
+
+# Reduce datasets to necessary columns only
+spe_n2k_2018 <- natura2000_spe_2018 %>% select(SITECODE, SPECIESCODE, SPECIESNAME, POPULATION)
+spe_n2k_2023 <- natura2000_spe_2023 %>% select(SITECODE, SPECIESCODE, SPECIESNAME, POPULATION)
+
+spe_n2k_2018 <- as.data.frame(spe_n2k_2018)
+spe_n2k_2023 <- as.data.frame(spe_n2k_2023)
+
+
+# Category 1: "remaining_in_site" ------------------------------------------
+# Sites and species area listed for 2023 and 2018 (no changes)
+# Species that are newly listed are not POPULATION D
+remaining_in_site <- spe_n2k_2023 %>%
+  inner_join(spe_n2k_2018, by = c("SITECODE", "SPECIESCODE"), suffix = c(".2023", ".2018"),relationship = "many-to-many") %>%
+  filter(POPULATION.2023 != "D" & POPULATION.2018 != "D") %>%
+  mutate(CATEGORY = "Remaining in Site")
+
+
+# Category 2: "Added to an existing sites" --------------------------------
+# Species have been listed for an existing site in 2023, and these newly listed species don’t belong to POPULATION “D”
+# Species were categorized as POPULATION "D" in 2018 but have now been listed and categorized as POPULATION A, B, C, or "”
+added_to_existing_site <- spe_n2k_2023 %>%
+  left_join(spe_n2k_2018, by = c("SITECODE", "SPECIESCODE"), suffix = c(".2023", ".2018"), relationship = "many-to-many") %>%
+  filter((is.na(POPULATION.2018) & POPULATION.2023 != "D") |
+           (POPULATION.2018 == "D" & !POPULATION.2023 %in% c("D"))) %>%
+  mutate(CATEGORY = "Added to an existing site")
+
+
+# category 3: "Added to a new site" ---------------------------------------
+# Sites and species were not listed in 2018 but in 2023 are newly listed 
+# Species that are newly listed are categorized as A, B, C, or ""
+
+added_to_a_new_site <-  spe_n2k_2023 %>% 
+  anti_join(spe_n2k_2018, by = "SITECODE") %>% 
+  semi_join(spe_n2k_2023, by = "SPECIESCODE") %>% 
+  filter(POPULATION %in% c("A", "B", "C", "")) %>%
+  mutate(CATEGORY = "Added to a new site") %>%
+  rename(POPULATION.2023 = POPULATION)
+
+# added_to_a_new_site <- spe_n2k_2023 %>%
+#   anti_join(spe_n2k_2018, by = c("SITECODE", "SPECIESCODE")) %>%
+#   filter(POPULATION %in% c("A", "B", "C", "")) %>%
+#   mutate(CATEGORY = "Added to a new site")%>%
+#   rename(POPULATION.2023 = POPULATION)
+
+names(added_to_a_new_site)
+
+# Category 4: "deleted from/with site" ------------------------------------
+# Sites and species were listed in 2018 but are not longer in 2023
+# Species that were in 2018 categorized as A, B, C or "" but have in 2023 been categorized as "D"
+
+# Criteria 1: Removal from Sites
+deleted_from_site <- spe_n2k_2018 %>%
+  anti_join(spe_n2k_2023, by = c("SITECODE", "SPECIESCODE")) %>%
+  mutate(CATEGORY = "Deleted from site") %>%
+  rename(POPULATION.2018 = POPULATION)
+
+names(deleted_from_site)
+
+# Criteria 2: Population Decline
+population_decline <- spe_n2k_2018 %>%
+  inner_join(spe_n2k_2023, by = c("SITECODE", "SPECIESCODE"), suffix = c(".2018", ".2023"), relationship = "many-to-many") %>%
+  filter(POPULATION.2018 %in% c("A", "B", "C", "") & POPULATION.2023 == "D") %>%
+  mutate(CATEGORY = "Population declined to D")
+
+names(population_decline)
+
+# Combine categories to form the final data set
+changes_species_1 <- bind_rows(
+  remaining_in_site,
+  added_to_existing_site,
+  population_decline)
+
+changes_species_2 <- bind_rows(
+  added_to_a_new_site,
+  deleted_from_site)
+
+# combine the geom from the changes species 1
+changes_species_1 <- changes_species_1 %>%
+  mutate(geom = coalesce(geom.2023, geom.2018)) %>%
+  select(-geom.2023, -geom.2018)
+
+
+# combine the two changes of species
+changes_species <- bind_rows(
+  changes_species_1,
+  changes_species_2)
+
+
+# consolidate the species names
+changes_species$Consolidated_Species <- ifelse(!is.na(changes_species$SPECIESNAME.2023), changes_species$SPECIESNAME.2023,
+                                               ifelse(!is.na(changes_species$SPECIESNAME.2018), changes_species$SPECIESNAME.2018,
+                                                      ifelse(!is.na(changes_species$SPECIESNAME), changes_species$SPECIESNAME, NA)))
+
+
+# delete columns we dont need 
+changes_species <- changes_species %>%
+  select(-SPECIESNAME.2018, -SPECIESNAME.2023, -SPECIESNAME) %>%
+  rename(SPECIESNAME = Consolidated_Species)
+
+
+# Convert to sf
+changes_species_sf <- st_as_sf(changes_species) 
+
+# Set the CRS to be sure 
+st_crs(changes_species_sf) <- 3035 
+
+# Save as a geopackage
+country_code <- unique(changes_species_sf$MS)
+geopackage_path <- file.path(output_file, paste0("natura2000_",country_code, "_species_changes_ETRS89.gpkg"))
+
+
+# Write the sf object to the GeoPackage
+st_write(changes_species_sf, geopackage_path, driver = "GPKG", overwrite = T)
+
+
+# Separate layers for each habitat code
+species_codes <- unique(changes_species_sf$SPECIESCODE)
+for (code in species_codes) {
+  species_data <- changes_species_sf[changes_species_sf$SPECIESCODE == code, ]
+  layer_name <- paste0("species_", code)
+  st_write(species_data, geopackage_path, layer = layer_name, driver = "GPKG", append = TRUE)
+}
+
+print("The combined changes have been saved as a GeoPackage.")
+
+
