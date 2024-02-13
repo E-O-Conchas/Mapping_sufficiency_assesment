95d31fa186ae0a9f4e896a6ab91b2cb7975e439f
Emma Oceguera-Conchas <113361856+E-O-Conchas@users.noreply.github.com>
2024-02-13 14:28:53 +0100

    has been modified

--- 01_scrip/natura2000_species_changes.R
+++ 01_scrip/natura2000_species_changes.R
@@ -6,73 +6,77 @@ gc()
 library(sf)
 library(dplyr)
 
-setwd("I:/biocon/Emmanuel_Oceguera/projects/2023_03_ETC_BE/Task 1.1.7.2 Protected areas dataflows/Subtask 2.viii Sufficiency assesment/outputs/PL")
+setwd("your work directory")
+getwd()
 
 # Define the paths to the data and the layer name
-gpkg_file_2018_path <- "I:/biocon/Emmanuel_Oceguera/projects/2023_03_ETC_BE/Task 1.1.7.2 Protected areas dataflows/Subtask 2.viii Sufficiency assesment/outputs/PL/Natura_2000_sites_v2018/species/natura_2000_PL_species_all_sites_ETRS89.gpkg"
-gpkg_file_2023_path <- "I:/biocon/Emmanuel_Oceguera/projects/2023_03_ETC_BE/Task 1.1.7.2 Protected areas dataflows/Subtask 2.viii Sufficiency assesment/outputs/PL/Natura_2000_sites_v2023/species/natura_2000_PL_species_all_sites_ETRS89.gpkg"
-output_file <- "Changes_Natura2000_sites_v2018_v2023/species"
+gpkg_file_2018_path <- "Path to the natura 2000 habitats that includes all the site, for the corresponding year"
+gpkg_file_2023_path <- "Path to the natura 2000 habitats that includes all the site, for the corresponding year" 
+output_file <- "Output folder"
 
 
-# Get the name fila
-layer_name <- st_layers(gpkg_file_2023_path)$name[1]
-
+# get the layer name
+layer_name <- st_layers(gpkg_file_2018_path)$name[1]
+layer_name_2 <- st_layers(gpkg_file_2023_path)$name[1]
 
 # Read habitats spatial data 2018 and 2023 for Poland
-natura2000_spe_2018 <- st_read(gpkg_file_2018_path, layer = layer_name) # every time when you run the function set the layers name
-natura2000_spe_2023 <- st_read(gpkg_file_2023_path, layer = layer_name) # every time when you run the function set the layers name
+natura2000_spe_2018 <- st_read(gpkg_file_2018_path, layer = layer_name) 
+natura2000_spe_2023 <- st_read(gpkg_file_2023_path, layer = layer_name_2)
 
-# # Convert to data frames
-# natura2000_spe_2018_df <- as.data.frame(natura2000_spe_2018)
-# natura2000_spe_2023_df <- as.data.frame(natura2000_spe_2023)
+# Convert to data frames
+spe_n2k_2018_df <- as.data.frame(spe_n2k_2018)
+spe_n2k_2023_df <- as.data.frame(spe_n2k_2023)
 
 # Reduce datasets to necessary columns only
-spe_n2k_2018 <- natura2000_spe_2018 %>% select(SITECODE, SPECIESCODE, SPECIESNAME, POPULATION)
-spe_n2k_2023 <- natura2000_spe_2023 %>% select(SITECODE, SPECIESCODE, SPECIESNAME, POPULATION)
+spe_n2k_2018_df <- natura2000_spe_2018 %>% select(SITECODE, SPECIESCODE, SPECIESNAME, POPULATION_CATEGORIES)
+spe_n2k_2023_df <- natura2000_spe_2023 %>% select(SITECODE, SPECIESCODE, SPECIESNAME, POPULATION_CATEGORIES)
 
-spe_n2k_2018 <- as.data.frame(spe_n2k_2018)
-spe_n2k_2023 <- as.data.frame(spe_n2k_2023)
+# Convert the columns as character to be able to make the futher joins
+spe_n2k_2018_df$SPECIESCODE <- as.character(spe_n2k_2018_df$SPECIESCODE)
+spe_n2k_2023_df$SPECIESCODE <- as.character(spe_n2k_2023_df$SPECIESCODE)
+
+# Free code for checking the data set before performing the categorization analysis
 
 
-# Category 1: "remaining_in_site" ------------------------------------------
+# Category 1: "remaining_in_site" 
 # Sites and species area listed for 2023 and 2018 (no changes)
 # Species that are newly listed are not POPULATION D
 remaining_in_site <- spe_n2k_2023 %>%
   inner_join(spe_n2k_2018, by = c("SITECODE", "SPECIESCODE"), suffix = c(".2023", ".2018"),relationship = "many-to-many") %>%
-  filter(POPULATION.2023 != "D" & POPULATION.2018 != "D") %>%
+  filter(POPULATION_CATEGORIES.2023 != "D" & POPULATION_CATEGORIES.2018 != "D") %>%
   mutate(CATEGORY = "Remaining in Site")
 
 
-# Category 2: "Added to an existing sites" --------------------------------
+# Category 2: "Added to an existing sites" 
 # Species have been listed for an existing site in 2023, and these newly listed species don’t belong to POPULATION “D”
 # Species were categorized as POPULATION "D" in 2018 but have now been listed and categorized as POPULATION A, B, C, or "”
-added_to_existing_site <- spe_n2k_2023 %>%
-  left_join(spe_n2k_2018, by = c("SITECODE", "SPECIESCODE"), suffix = c(".2023", ".2018"), relationship = "many-to-many") %>%
-  filter((is.na(POPULATION.2018) & POPULATION.2023 != "D") |
-           (POPULATION.2018 == "D" & !POPULATION.2023 %in% c("D"))) %>%
+# Identify sites that are new in 2023 by using `anti_join` to find SITECODES that do not exist in 2018 data.
+new_sites_2023 <- spe_n2k_2023_df %>%
+  anti_join(spe_n2k_2018_df, by = "SITECODE")
+
+# For the existing sites, make sure to exclude the newly identified sites.
+existing_sites_2023 <- spe_n2k_2023_df %>%
+  filter(!SITECODE %in% new_sites_2023$SITECODE)
+
+added_to_existing_site <- existing_sites_2023 %>%
+  left_join(spe_n2k_2018_df, by = c("SITECODE", "SPECIESCODE"), suffix = c(".2023", ".2018"), multiple = 'all') %>%
+  filter((is.na(POPULATION_CATEGORIES.2018) & POPULATION_CATEGORIES.2023 != "D") |
+           (POPULATION_CATEGORIES.2018 == "D" & !POPULATION_CATEGORIES.2023 %in% c("D"))) %>%
   mutate(CATEGORY = "Added to an existing site")
 
 
-# category 3: "Added to a new site" ---------------------------------------
+# category 3: "Added to a new site" 
 # Sites and species were not listed in 2018 but in 2023 are newly listed 
 # Species that are newly listed are categorized as A, B, C, or ""
 
-added_to_a_new_site <-  spe_n2k_2023 %>% 
-  anti_join(spe_n2k_2018, by = "SITECODE") %>% 
-  semi_join(spe_n2k_2023, by = "SPECIESCODE") %>% 
-  filter(POPULATION %in% c("A", "B", "C", "")) %>%
-  mutate(CATEGORY = "Added to a new site") %>%
-  rename(POPULATION.2023 = POPULATION)
+# Use `filter` to further refine to new habitats that are not marked as "D".
+added_to_a_new_site <- new_sites_2023 %>%
+  filter(POPULATION_CATEGORIES %in% c("A", "B", "C", "no data")) %>%
+  mutate(CATEGORY = "Added to a new site") %>% 
+  rename(POPULATION_CATEGORIES.2023 = POPULATION_CATEGORIES)
 
-# added_to_a_new_site <- spe_n2k_2023 %>%
-#   anti_join(spe_n2k_2018, by = c("SITECODE", "SPECIESCODE")) %>%
-#   filter(POPULATION %in% c("A", "B", "C", "")) %>%
-#   mutate(CATEGORY = "Added to a new site")%>%
-#   rename(POPULATION.2023 = POPULATION)
 
-names(added_to_a_new_site)
-
-# Category 4: "deleted from/with site" ------------------------------------
+# Category 4: "deleted from/with site" 
 # Sites and species were listed in 2018 but are not longer in 2023
 # Species that were in 2018 categorized as A, B, C or "" but have in 2023 been categorized as "D"
 
@@ -80,17 +84,15 @@ names(added_to_a_new_site)
 deleted_from_site <- spe_n2k_2018 %>%
   anti_join(spe_n2k_2023, by = c("SITECODE", "SPECIESCODE")) %>%
   mutate(CATEGORY = "Deleted from site") %>%
-  rename(POPULATION.2018 = POPULATION)
+  rename(POPULATION_CATEGORIES.2017 = POPULATION_CATEGORIES)
 
-names(deleted_from_site)
 
 # Criteria 2: Population Decline
 population_decline <- spe_n2k_2018 %>%
   inner_join(spe_n2k_2023, by = c("SITECODE", "SPECIESCODE"), suffix = c(".2018", ".2023"), relationship = "many-to-many") %>%
-  filter(POPULATION.2018 %in% c("A", "B", "C", "") & POPULATION.2023 == "D") %>%
+  filter(POPULATION_CATEGORIES.2018 %in% c("A", "B", "C", "") & POPULATION_CATEGORIES.2023 == "D") %>%
   mutate(CATEGORY = "Population declined to D")
 
-names(population_decline)
 
 # Combine categories to form the final data set
 changes_species_1 <- bind_rows(
